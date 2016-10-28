f3532fd08af2701801abae54660ce49c17656b8e
Daniel Pape <danielwpape@gmail.com>
2016-10-28 22:14:38 +0100

    Added Comments Style

--- app/components/Scape.jsx
+++ app/components/Scape.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 
-var Scape = (props) =>{
+var Scape = ({image}) =>{
   return(
     <div>
       <div className="scape">
@@ -8,10 +8,17 @@ var Scape = (props) =>{
           <img className="scapeProfileImage" src={'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/14519821_10153837134245952_3854936125472397001_n.jpg?oh=b00518032d1c1ba0f7ed9959cbad93ee&oe=5899B507'}/><div className="ScapeText">Daniel Pape<a className="ScapeDate">10.23 22-OCT-2016</a></div>
         </div>
         <div>
-          <img src={'https://lh3.googleusercontent.com/ls7kSgqkR5QOHwhiyrc50OcbFltXHdfAtElIE6pGL4yIL0u3bq8oowyy0s1BHmhLuEph5QfkW24gsCWF4fxn6PxlHVP8kpELLIBnNQZPT-wEAp716TaT-EzNYwZGb2plJB0Sai8SubjIIDlzcICFG8NZFkhJR0M8LqR1uySurDXhkqBdDzq6m6oI3MQGN-9GsHIXqumXhSViFn5gX_XTZmE-Z7PcqbYYaqjsizxD6GIy7BH9FFwoa5tL7jwGLHeFPTPcIZW-_5eoWggBkGJyfzfL1G23RGP3VJE0UrJDoTEc9hF-R3QvEIeoyVc57unqFbrUX69B8zJEcZ90Z1IVh2PwrEURpDnY7EdmsE-sWhv-yfDkILr9_umE3RBqITXRgWYVHWSyT8eSPz9xX6jMd6Suakx8e8Fc_z5DfGuIsS2-pYG8z5_SAjVpePWnipMpEJT3synMBvWpOY3LrLtfg1GzsVNAYHsXI6EfDRLwgVI-y_zYxhspqztu5PBF34zPYVDsYwhJjkW1aWnwtQorgyXF22b9kssCCa66Pl0PSFMWJbClw-h4r3aOrtfXbJQsX9ASfZUA-rCbWbXqCDlgQTOw3tTxcHihgvImln0MVWGz5O3e=w1089-h817-no'}/>
+          <img className="scapeImage" src={image}/>
         </div>
         <div className="scapeFooter">
           <div className="ScapeText">Sunrise 07.52 Sunset 17.40<a className="ScapeDate">9&deg;C</a></div>
+        </div>
+        <div className="scapeMeta">
+          <div className="ScapeText">
+            <p>Liked by Daniel Pape</p>
+            <p><strong>Daniel Pape</strong> Great shot!</p>
+          </div>
+          <div className="ScapeText">+ Add Comment<a className="ScapeDate">Like</a></div>
         </div>
       </div>
     </div>
